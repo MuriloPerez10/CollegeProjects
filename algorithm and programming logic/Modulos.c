4b1a568a48dd4820fba04f409f57151fd54b4def
Murilo Perez <62762784+MuriloPerez10@users.noreply.github.com>
2021-09-23 16:34:11 -0300

    IntroductionInModulo
    create program with modulo

---
+++ algorithm and programming logic/Modulos.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int a,b,c,r,total;
+
+//funçao para realizar a adiçao
+int Adicao(){
+    r = a+b;
+    return(r);
+}
+
+//procedimento para entrada de dados
+void dado(){
+    printf("\n escreva um numero: ");
+    scanf("%d",&a);
+    printf("\n escreva um numero: ");
+    scanf("%d",&b);
+}
+
+//programa para interação com o usuario
+int main()
+{
+    printf("quer calcular? (0)sim  (1)nao");
+    scanf("%d",&c);
+    if(c == 0){
+        dado();
+        total = Adicao();
+        printf("total-> (%d)",total);
+    }else{
+        printf("ate a proxima");
+    };
+    return 0;
+}
